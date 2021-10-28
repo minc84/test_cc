30c174e5c80d6094330493a0349a3c9869fa9657
minc84 <nesterovish@yandex.ru>
2021-10-29 01:32:48 +0300

    test

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/")
+@application.route("/")
 def index():
 	return render_template('index.html')
 
 
 
 if __name__ == "__main__":
-	app.run(debug=True)
+	application.debug = True
+	application.run()
